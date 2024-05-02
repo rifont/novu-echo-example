cb516d8a20310e73d88158830fc9142328d66805
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-05-02 16:52:38 +0100

    fix: subscriber id

--- components/NovuProvidedNC.tsx
+++ components/NovuProvidedNC.tsx
@@ -1,10 +1,11 @@
 import { NovuProvider } from "@novu/notification-center";
 import NovuNC from "./NovuNC";
+import { SUBSCRIBER_ID } from "@/constants/subscriber";
 
 export const NovuProvidedNC = () => {
     return (
       <NovuProvider
-        subscriberId={"richard@fontein.co"}
+        subscriberId={SUBSCRIBER_ID}
         applicationIdentifier={process.env.NEXT_PUBLIC_NOVU_APP_ID as string}
       >
         <NovuNC/>
