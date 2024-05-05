dea83b831a84b3e2251ffd88f96775f49f52d2b3
Richard Fontein <32132657+rifont@users.noreply.github.com>
2024-05-05 17:55:17 +0100

    style: Add slide effect and icons to notification center actions

--- components/notifications/CustomNotificationCenter.tsx
+++ components/notifications/CustomNotificationCenter.tsx
@@ -4,11 +4,23 @@ import { Skeleton } from "../ui/skeleton";
 import React from "react";
 import { cx } from "class-variance-authority";
 import { Button } from "../ui/button";
+import { icons } from "lucide-react";
 
 const LOADING_NOTIFICATIONS_COUNT = 5;
 
 export const CustomNotificationCenter = () => {
-  const { notifications = [], isLoading, hasNextPage, fetchNextPage, markNotificationAsRead, markNotificationAsUnRead, refetch, markAllNotificationsAsRead } = useNotifications();
+  const {
+    notifications = [],
+    isLoading,
+    hasNextPage,
+    fetchNextPage,
+    markNotificationAsRead,
+    isFetching,
+    markNotificationAsUnRead,
+    refetch,
+    markAllNotificationsAsRead,
+    removeMessage,
+  } = useNotifications();
   const { data: unreadCount } = useUnreadCount();
 
   React.useEffect(() => {
@@ -18,27 +30,34 @@ export const CustomNotificationCenter = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [unreadCount]);
 
-  const actions = [
+  const actions: { label: string, icon: keyof typeof icons, onClick: (id: string) => void }[] = [
     {
       label: 'Read',
+      icon: 'Check',
       onClick: (id: string) => markNotificationAsRead(id)
     },
     {
       label: 'Unread',
+      icon: 'MessageSquareDot',
       onClick: (id: string) => markNotificationAsUnRead(id)
     },
-  ]
+    {
+      label: 'Archive',
+      icon: 'Archive',
+      onClick: (id: string) => removeMessage(id)
+    }
+  ];
 
   return (
     <div>
       <div className="flex flex-row justify-between mb-2">
         <h3 className="text-2xl font-bold">Notifications</h3>
         <Button
-        size='sm'
-        className="w-24 h-8 text-xs"
-        variant={'outline'}
-        onClick={()=> markAllNotificationsAsRead()}>
-        Read All</Button>
+          size='sm'
+          className="w-24 h-8 text-xs"
+          variant={'outline'}
+          onClick={() => markAllNotificationsAsRead()}>
+          Read All</Button>
       </div>
       <InfiniteScroll
         className="flex flex-col gap-2 overflow-y-auto w-full border rounded-md p-3 shadow-inner no-scrollbar"
@@ -55,30 +74,35 @@ export const CustomNotificationCenter = () => {
         }
       >
         {notifications.map((notification) => (
-          <div key={notification._id} className={cx("flex flex-row gap-2 border rounded-md p-2", !notification.read && "bg-foreground/10")}>
-            <p className="w-[100px] text-sm">{new Date(notification.createdAt).toLocaleTimeString()}</p>
-            <p className="w-full">{notification.content as string}</p>
-            <div className="flex flex-col gap-2 content-end">
+          <div key={notification._id} className={cx("relative flex flex-row gap-2 border rounded-md p-2 min-h-28", !notification.read && "bg-foreground/10")}>
+            <p className="min-w-14 text-sm text-primary/50">{new Date(notification.createdAt).toLocaleTimeString()}</p>
+            <p className="w-full mr-8">{notification.content as string}</p>
+            <div className="flex flex-col gap-2 items-end absolute right-2">
               {actions.map((action, index) => (
                 <Button
                   key={index}
                   variant={'outline'}
                   size='sm'
-                  className="rounded-md text-xs h-6 w-16 px-2"
+                  className="relative flex overflow-hidden text-xs rounded-md w-6 hover:w-20 h-6 p-1 group transition-all duration-300 text-primary/50 hover:text-primary"
                   onClick={() => action.onClick(notification._id)}>
-                  {action.label}
+                  <div className="flex flex-row mr-auto">
+                    {React.createElement(icons[action.icon], { className: 'h-4 w-4' })}
+                    <span className="px-2 whitespace-nowrap group-hover:translate-x-0 translate-x-full absolute right-0 transition-transform duration-300">
+                      {action.label}
+                    </span>
+                  </div>
                 </Button>
               ))}
             </div>
           </div>
         ))}
-        {isLoading && Array.from({ length: LOADING_NOTIFICATIONS_COUNT }).map((_, i) => (
+        {(isLoading || isFetching) && Array.from({ length: LOADING_NOTIFICATIONS_COUNT }).map((_, i) => (
           <div key={i} className="flex flex-row gap-2 border border-foreground/10 rounded-md p-2">
             <Skeleton className="w-[100px] h-6" />
             <Skeleton className="w-full h-12" />
             <div className="flex flex-col gap-2 content-end">
               {actions.map((action, index) => (
-                <Skeleton key={index} className="h-6 w-16 px-2" />
+                <Skeleton key={index} className="h-6 w-6 px-2" />
               ))}
             </div>
           </div>
